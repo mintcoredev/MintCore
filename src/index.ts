380c590f400b3559f637111c71ff9cbc1e4d4789
mintcoredev <mintcoredev@proton.me>
2026-02-18 14:26:21 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,6 @@
-export * from './minting/mintFungibleToken';
-export * from './minting/mintNFT';
-export * from './minting/buildMintTransaction';
+export { MintEngine } from "./core/MintEngine";
+export { LibauthAdapter } from "./adapters/LibauthAdapter";
 
-export * from './verification/verifyMint';
-export * from './verification/decodeTokenMetadata';
-export * from './verification/validateMetadata';
-
-export * from './metadata/createMetadata';
-export * from './metadata/encodeMetadata';
-export * from './metadata/decodeMetadata';
-
-export * from './utils/estimateFees';
-export * from './utils/getTokenIdFromTx';
-export * from './utils/getLibauthVersion';
+export * from "./types/MintConfig";
+export * from "./types/TokenSchema";
+export * from "./core/MintResult";
