70f509c574c53aa81e68a8eaa21f3dd9d3944d5d
mintcoredev <mintcoredev@proton.me>
2026-02-18 13:47:21 -0500

    Create src/index.ts with initial exports

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+export * from './minting/mintFungibleToken';
+export * from './minting/mintNFT';
+export * from './minting/buildMintTransaction';
+
+export * from './verification/verifyMint';
+export * from './verification/decodeTokenMetadata';
+export * from './verification/validateMetadata';
+
+export * from './metadata/createMetadata';
+export * from './metadata/encodeMetadata';
+export * from './metadata/decodeMetadata';
+
+export * from './utils/estimateFees';
+export * from './utils/getTokenIdFromTx';
+export * from './utils/getLibauthVersion';
