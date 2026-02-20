983195f48f095450186d08c68e4ca9e6a42e2076
mintcoredev <mintcoredev@proton.me>
2026-02-20 14:18:56 -0500

    Update LibauthAdapter.ts

--- src/adapters/LibauthAdapter.ts
+++ src/adapters/LibauthAdapter.ts
@@ -1,14 +1,20 @@
 import { MintConfig } from "../types/MintConfig";
 import { TokenSchema } from "../types/TokenSchema";
+import { TransactionBuilder } from "../core/TransactionBuilder";
 
 export class LibauthAdapter {
-  constructor(private config: MintConfig) {}
+  private builder: TransactionBuilder;
+
+  constructor(private config: MintConfig) {
+    this.builder = new TransactionBuilder(config);
+  }
 
   async buildMintTransaction(schema: TokenSchema): Promise<{ hex: string; txid: string }> {
-    // Placeholder until Libauth integration
+    const tx = await this.builder.build(schema);
+
     return {
-      hex: "00",
-      txid: "placeholder-txid"
+      hex: tx.hex,
+      txid: tx.txid
     };
   }
 }
